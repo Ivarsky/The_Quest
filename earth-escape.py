a50bc25038c89ae52d936c791bdaf3ec818a3066
Ivarsky <iamh90@gmail.com>
2022-08-07 19:32:29 +0200

    feat: triangulo representando la nave dibujado

--- earth-escape.py
+++ earth-escape.py
@@ -3,20 +3,22 @@ import pygame
 
 class EarthEscape:
 
-    _ALTO = 200
-    _ANCHO = 320
+    _HEIGHT = 200
+    _WIDTH = 320
 
     def __init__(self):
-        print("contruyendo un objeto EarthEscape")
+        print("building object EarthEscape")
         pygame.init()
-        self.pantalla = pygame.display.set_mode((self._ANCHO, self._ALTO))
+        self.screen = pygame.display.set_mode((self._WIDTH, self._HEIGHT))
 
-    def bucle_principal(self):
-        print("estoy en el bucle principal")
+    def main_loop(self):
+        print("in main loop")
+        triangle_ship = [[10, 20], [10, 30], [30, 25]]
         while True:
+            pygame.draw.polygon(self.screen, (255, 255, 255), triangle_ship)
             pygame.display.flip()
 
 
 if __name__ == "__main__":
-    juego = EarthEscape()
-    juego.bucle_principal()
+    game = EarthEscape()
+    game.main_loop()
